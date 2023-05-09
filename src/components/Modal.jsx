a38dde006217a729471df54806808d7a03541a8d
Bhathiya Kariyawasam <bhathiyakariyawasam94@gmail.com>
2023-05-10 01:27:39 +0530

    font changes

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -11,7 +11,7 @@ const Modal = (
    return (
       <div className="bg-white/70 fixed top-0 left-0 w-full h-full z-50">
          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-fit rounded-lg border overflow-hidden bg-white">
-            <div className="flex flex-row justify-between items-center px-4 py-3 bg-primaryBlue uppercase font-poppins font-medium text-white text-lg">
+            <div className="flex flex-row justify-between items-center px-4 py-3 bg-primaryBlue uppercase font-poppins font-medium text-white">
                <span>fit instructions</span>
                <FontAwesomeIcon className="cursor-pointer" icon={faXmark} />
             </div>
