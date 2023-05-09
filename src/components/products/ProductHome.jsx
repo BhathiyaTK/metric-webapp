558caa1491ef983cd8824d5bdbd759a28d445152
Bhathiya Kariyawasam <bhathiyakariyawasam94@gmail.com>
2023-05-10 01:35:46 +0530

    Fix duplicate id issue in product page

--- src/components/products/ProductHome.jsx
+++ src/components/products/ProductHome.jsx
@@ -32,7 +32,8 @@ const ProductHome = () => {
                   </h2>
                   <Link to={card.path} aria-label={'See all products' + index} title="Link to see all products">
                      <button title="See all products" aria-label={'See All Products' + index}
-                        className="font-poppins uppercase font-semibold text-white text-2xl rounded-xl px-5 py-1 bg-primaryGreen hover:bg-primaryBlue transition-all" id="seeAllProductsButton">
+                        className="font-poppins uppercase font-semibold text-white text-2xl rounded-xl px-5 py-1 bg-primaryGreen hover:bg-primaryBlue transition-all"
+                        id={'seeAllProductsButton' + index}>
                         See all products
                      </button>
                   </Link>
