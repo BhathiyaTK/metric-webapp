062ddf373df263eb54978e59ccc067ec78cf72bd
Bhathiya Kariyawasam <bhathiyakariyawasam94@gmail.com>
2023-05-11 15:28:10 +0530

    Add changes to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,7 @@ import { productCards } from '../constants';
 const Home = () => {
     return (
         <div role="presentation" className="custom-container m-auto h-full">
+            <h1 className="invisible">Metric home page</h1>
             <div role="main" className="block md:flex md:flex-row items-start justify-center md:space-x-16 absolute top-56 md:top-1/2 left-1/2 -translate-x-1/2 md:-translate-y-1/2 w-4/5">
                 {productCards.map((card, index) => (
                     <div role="presentation" className="w-full md:w-1/3 text-center mb-16 md:mb-0" key={index}>
@@ -17,11 +18,11 @@ const Home = () => {
                         </div>
                         <div role="contentinfo" aria-label='Three main product categories of metric' className="bg-primaryGreen px-2 py-3 rounded-3xl mt-10 md:mt-20">
                             <Link to={'products/' + card.path} aria-label={card.name} role="link">
-                                <h1 className="uppercase text-linkBlack hover:text-primaryBlue transition-all font-poppins font-semibold text-2xl md:text-3xl lg:text-4xl mb-2">
+                                <h2 className="uppercase text-linkBlack hover:text-primaryBlue transition-all font-poppins font-semibold text-2xl md:text-3xl lg:text-4xl mb-2">
                                     {card.name}
-                                </h1>
+                                </h2>
                             </Link>
-                            <h2 className="uppercase font-poppins font-medium text-2xl text-linkBlack">{card.subName}</h2>
+                            <h3 className="uppercase font-poppins font-medium text-2xl text-linkBlack">{card.subName}</h3>
                         </div>
                     </div>
                 ))}
