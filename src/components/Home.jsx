0dfdf6730453a5b2c10bc190b2e457afb782bdff
Bhathiya Kariyawasam <bhathiyakariyawasam94@gmail.com>
2023-05-11 15:41:22 +0530

    Add changes to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,11 +18,11 @@ const Home = () => {
                         </div>
                         <div role="contentinfo" aria-label='Three main product categories of metric' className="bg-primaryGreen px-2 py-3 rounded-3xl mt-10 md:mt-20">
                             <Link to={'products/' + card.path} aria-label={card.name} role="link">
-                                <h1 className="uppercase text-linkBlack hover:text-primaryBlue transition-all font-poppins font-semibold text-2xl md:text-3xl lg:text-4xl mb-2">
+                                <div className="uppercase text-linkBlack hover:text-primaryBlue transition-all font-poppins font-semibold text-2xl md:text-3xl lg:text-4xl mb-2">
                                     {card.name}
-                                </h1>
+                                </div>
                             </Link>
-                            <h2 className="uppercase font-poppins font-medium text-2xl text-linkBlack">{card.subName}</h2>
+                            <div className="uppercase font-poppins font-medium text-2xl text-linkBlack">{card.subName}</div>
                         </div>
                     </div>
                 ))}
